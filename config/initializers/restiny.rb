88d8dd3a332875469de4da95c3776ccd54da7b30
Menno Kramer <mennockramer@gmail.com>
2023-07-01 14:35:03 +0100

    Removed server-only DESTINY_MANIFEST setup

--- config/initializers/restiny.rb
+++ config/initializers/restiny.rb
@@ -4,9 +4,7 @@ Restiny.api_key = Rails.application.credentials.bungie.api_key
 Restiny.oauth_client_id = Rails.application.credentials.bungie.oauth_client_id
 
 Rails.application.config.after_initialize do
-    if defined?(::Rails::Server) # only gets mainfest when booting server
         puts "Getting Destiny manifest..."
         DESTINY_MANIFEST = Restiny.download_manifest()
-        puts "DESTINY_MANIFEST set up"
-    end 
+        puts "DESTINY_MANIFEST set up" 
 end
